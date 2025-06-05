1d854f63ad7f427508a29b5250b91e6af92c5ae9
Natalia Soledad Gutierrez <nataliasolegutierrez@gmail.com>
2025-06-05 14:01:06 +0200

    se agrega click

--- tests/practica.spec.ts
+++ tests/practica.spec.ts
@@ -3,5 +3,6 @@ import { test, expect } from '@playwright/test';
 test('test input placeholder', async ({ page }) => {
   await page.goto('https://opencart.abstracta.us/');
   await page.getByPlaceholder('search').fill('iphone');
+  await page.locator('button[type="button"].btn.btn-default.btn-lg').click();
   await page.pause();
 });
