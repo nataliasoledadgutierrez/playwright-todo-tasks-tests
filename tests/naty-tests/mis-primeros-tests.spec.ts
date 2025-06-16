27b4336280f22a5ba183bc058a87d1841d0cba8d
Natalia Soledad Gutierrez <nataliasolegutierrez@gmail.com>
2025-06-16 20:10:18 +0200

    Verificar que la tarea este con la clase completed y con el check activado

--- tests/naty-tests/mis-primeros-tests.spec.ts
+++ tests/naty-tests/mis-primeros-tests.spec.ts
@@ -4,7 +4,6 @@ test ('Agregar nueva tarea', async ({page}) => {
     //1. ir a la pàgiana
     await page.goto('https://demo.playwright.dev/todomvc/#/');
 
-
     //2. Seleccionar el campo de texto para agregar una tarea
     const inputNuevaTarea = page.locator('.new-todo');
 
@@ -15,8 +14,15 @@ test ('Agregar nueva tarea', async ({page}) => {
     await inputNuevaTarea.press('Enter')
     
       //6. Verificar tarea agregada.
-    const tareaAgregada= page.locator('.todo-list li', { hasText:'Productos de limpieza'});
+    const tarea= page.locator('.todo-list li', { hasText:'Productos de limpieza'});
 
     //7. Verificar que la tarea se haya agregado correctamente
-    await expect(tareaAgregada).toBeVisible();
+    await expect(tarea).toBeVisible();
+
+    //8. Hacer click a tarea para marcarla como completada
+    const checkbox = tarea.locator('.toggle'); // checkbox para marcar completada
+    await checkbox.click();
+    
+    //9 verificar que la tarea tenga la clase 'completed' que indica que esta completada
+    await expect (tarea).toHaveClass('completed');
 });
