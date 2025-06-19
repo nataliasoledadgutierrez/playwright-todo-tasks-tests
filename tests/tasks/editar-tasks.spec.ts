04d35a27cca3fb33dbbfee42f79403a43a9ac338
Natalia Soledad Gutierrez <nataliasolegutierrez@gmail.com>
2025-06-19 17:54:38 +0200

    Agrego test para editar tarea

---
+++ tests/tasks/editar-tasks.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+
+test('Editar tarea existente', async ({ page}) => {
+    //1. Ir a la página
+    await page.goto('https://demo.playwright.dev/todomvc/#/');
+    //2. Agregar una tarea
+    const inputNuevaTarea = page.locator('.new-todo');
+    //3.Escribir nueva tarea
+    await inputNuevaTarea. fill('Tarea para editar');
+    //4. Agregar tarea
+    await inputNuevaTarea.press('Enter')
+    //4. Verificar que la tarea se haya agregado correctamente
+    const tarea = page.locator('.todo-list li', {hasText: 'Tarea para editar'});
+    await expect(tarea).toBeVisible();
+    //5.Hacer doble click para editar la tarea
+    await tarea.dblclick();
+    //6. Seleccionar el campo de edición
+    const inputEdicion = page.locator('.edit');
+    //7. Escribir nueva tarea
+    await inputEdicion.fill('Tarea editada');
+    //8. Presionar enter patra guardar los cambios
+    await inputEdicion.press('Enter');
+    //9. Verificar que la taraea se haya editado correctamente
+    const tareaEditada = page.locator('.todo-list li', { hasText: 'Tarea editada' });
+    await expect(tareaEditada).toBeVisible();
+    //10. Verificar que la tarea original ya no este viisible
+    await expect(tarea).not.toBeVisible();
+})
